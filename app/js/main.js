778c9a302ccb37c80e44a6cde4e044af1e637d45
Roman <55654942+romanprokopuk@users.noreply.github.com>
2022-08-25 00:22:20 +0300

    1

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,23 @@ $('.close-img__ppmenu').on('click', function(){
 $('.menu-href__ppmenu').on('click', function(){
   $('.pop-up-wrap__ppmenu').toggleClass('display-block');
 })
+
+$('.why-contant-elem__price').on('click', function(){
+  $('.why-contant-elem-clarification__price').toggleClass('display-block-flex');
+})
+
+$('.why-contant-elem__time').on('click', function(){
+  $('.why-contant-elem-clarification__time').toggleClass('display-block-flex');
+})
+
+$('.why-contant-elem__carefully').on('click', function(){
+  $('.why-contant-elem-clarification__carefully').toggleClass('display-block-flex');
+})
+
+$('.why-contant-elem__politely').on('click', function(){
+  $('.why-contant-elem-clarification__politely').toggleClass('display-block-flex');
+})
+
+$('.why-contant-elem__experience').on('click', function(){
+  $('.why-contant-elem-clarification__experience').toggleClass('display-block-flex');
+})
